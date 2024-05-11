916832880332609f32d4f93e734ad87a93757cb1
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-05-11 18:24:54 +0530

    logical part added for log in

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+function custName(){
+    const nameElement = document .getElementById("header-heading")
+    const name =prompt("Please Enter Your Name To Log Into Our Site")
+    nameElement.innerText=`Hello ${name} , Welcome To Shri Sadguru Mobile Shop Website.`;
+    
+}    
+
