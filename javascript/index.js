45cc2d224be8f209f6d65693295f344896677db1
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-05-14 21:09:21 +0530

    some logical part added for change image

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 function custName(){
     const nameElement = document .getElementById("header-heading")
-    const name =prompt("Please Enter Your Name To Log Into Our Site")
+    const name = prompt ("Please Enter Your Name To Log Into Our Site")
     nameElement.innerText= `Hello ${name} , Welcome To Shri Sadguru Mobile Shop Online Platform.`;
     
 }    
