22ccb5fa7754bd69121a0e474637f8f342d5ba5d
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-05-14 15:45:22 +0530

    logical part added for chnaging offers

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,42 @@ function custName(){
     nameElement.innerText=`Hello ${name} , Welcome To Shri Sadguru Mobile Shop Website.`;
     
 }    
+function imageChange (){
+    const imageElement =document.getElementById("offer2-cards")
+   const min = 1;
+   const max = 7;
+   const mathElement = Math.round (Math.random() * (max-min) + (min));
 
+   console.log(mathElement)
+   switch(mathElement){
+    case(1) :
+    imageElement.src = "./img/home-img/home-img3.jpg";
+    break;
 
+    case(2) :
+    imageElement.src = "./img/home-img/home-img2.jpg";
+    break;
+        
+    case(3) :
+    imageElement.src = "./img/home-img/home-img4.jpg";
+    break;
+        
+    case(4) :
+    imageElement.src = "./img/home-img/home-img5.jpg";
+    break;
+        
+    case(5) :
+    imageElement.src = "./img/home-img/home-img6.jpg";
+    break;
+        
+    case(6) :
+    imageElement.src = "./img/home-img/home-img7.jpg";
+    break;
+
+    case(7) :
+    imageElement.src = "./img/home-img/home-img8.jpg";
+    break;
+        
+   }
+}
 
